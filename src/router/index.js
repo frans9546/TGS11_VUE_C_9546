46f4d7432b96e0873f6e1988aee1afdfbd8bc2d7
franskristian <franskristian76@gmail.com>
2019-11-20 23:35:53 +0700

    my first commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+    
+import Vue from 'vue' 
+import Router from 'vue-router' 
+
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue') 
+
+
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`) 
+}
+const routes = [ 
+    { 
+       
+        path: '/', 
+        component: DashboardLayout, 
+        children: [ 
+            {
+                name: 'loginController',
+                path: '/login',
+                component: loadView('loginController')
+            },
+            { 
+                name: 'UserController', 
+                path: '/users', 
+                component: loadView('userController') 
+            },
+            { 
+                name: 'branchesController',            
+                path: '/branches', 
+                component: loadView('branchesController') 
+            } 
+        ] 
+    }, 
+] 
+Vue.use(Router) 
+
+const router = new Router({mode: 'history', routes: routes}) 
+    
+export default router
